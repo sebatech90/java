226f4155735a9ecd86701894ba89927d9820099f
sebatech90 <airforce.edu@gmail.com>
2018-08-26 14:08:37 +0200

    add exceptions

--- src/ChessEngine.java
+++ src/ChessEngine.java
@@ -1,41 +1,58 @@
 import wp.example.chess.ChessDashboard;
+import wp.example.chess.testExceptions;
+
 import java.util.Scanner;
 //import wp.example.chess.ChessPiece;
 import java.util.HashMap;
 
 public class ChessEngine {
     public static void main(String[] args) {
-        System.out.println("Hello! \n");
+        System.out.println("GAME STARTED! \n");
         ChessDashboard dashboard1 =  new ChessDashboard();
+
+        //testExceptions test = new testExceptions();
 
         //System.out.println("Chess" +  dashboard1.getDashboardField(0,0));
         //Move w
-        dashboard1.moveDashboardField(1, 0, 3, 0);
-
-        //Move b
-        dashboard1.moveDashboardField(6, 1, 4, 1);
-        dashboard1.moveDashboardField(4, 1, 2, 1);
-        //Attack
-        dashboard1.moveDashboardField(0, 0, 2, 0);
-        dashboard1.moveDashboardField(2, 0, 2, 1);
+//        dashboard1.moveDashboardField(1, 0, 3, 0);
+//
+//        //Move b
+//        dashboard1.moveDashboardField(6, 1, 4, 1);
+//        dashboard1.moveDashboardField(4, 1, 2, 1);
+//        //Attack
+//        dashboard1.moveDashboardField(0, 0, 2, 0);
+//        dashboard1.moveDashboardField(2, 0, 2, 1);
         //dashboard1.moveDashboardField(0, 0, 4, 0);
 
         //dashboard1.showDashboardFieldsId();
+        //dashboard1.showDashboardFields();
+
+        int movesCounter = 0;
+        int movesToken = 0;
+        String playerMove;
+        String[] Players = dashboard1.getPlayerType();
+        Scanner cmdMove = new Scanner(System.in);
         dashboard1.showDashboardFields();
 
-        int mainCounter = 0;
+        while (dashboard1.findKings() == 2 && movesCounter < 1000) {
 
-        while (dashboard1.findKings() == 2) {
+            System.out.println(Players[movesToken] + "`S MOVE");
+            playerMove = cmdMove.nextLine();
+            System.out.println("try turn to " + playerMove + " It`s " + movesCounter + " move number");
 
-            String turn;
+            //dashboard.moveDashboardField(A1,D5);
+            String coordinates[] = playerMove.split(" ");
 
-            Scanner cmdMove = new Scanner(System.in);
-
-            turn = cmdMove.nextLine();
-
-            System.out.println("turn " + mainCounter);
-
-            mainCounter++;
+            try {
+                if (dashboard1.movePiece(coordinates[0], coordinates[1], Players[movesToken]) == 0) {
+                    movesToken = 1 - movesToken;
+                    dashboard1.showDashboardFields();
+                    movesCounter++;
+                }
+            } catch (Exception e) {
+                System.out.println("Something went wrong while moving. Repeat the moving.");
+                e.printStackTrace();
+            }
         }
     }
 }
